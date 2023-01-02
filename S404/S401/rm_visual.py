a05ea46a5ab70b6ceaa77e84d7e100f6fa764660
Foo0s <oraproavp@list.ru>
2023-01-02 12:36:22 +0300

    Lesson №5 matplotlib.

--- S404/S401/rm_visual.py
+++ S404/S401/rm_visual.py
@@ -1,20 +1,42 @@
 # Импортирование...
 import matplotlib.pyplot as plt
+import random
 
 from random_numb import RandomWalk
 
-# Построение случайного блуждания.
-rw = RandomWalk()
-rw.fill_walk()
+# Генерирование новых блужданий.
+while True:
+    # Построение случайного блуждания.
+    rw = RandomWalk(random.randint(5000, random.randint(5001, 50000)))
+    rw.fill_walk()
 
-# Нанесение точек на диаграмму.
+    # Нанесение точек на диаграмму.
 
-# // Стиль.
-plt.style.use("ggplot")
+    # // Стиль.
+    plt.style.use("ggplot")
 
-# // Подключение.
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=8)
+    # // Подключение.
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    point_numbers = range(rw.number_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Oranges,
+               edgecolors="none", s=2.7)      # edgecolors -> удаление черного контура.
 
-# // Запуск.
-plt.show()
+    # // Вывод первой точки.
+    ax.scatter(rw.x_values[0], rw.y_values[0], c='blue', edgecolors="green", s=101)
+
+    # // Вывод последней точки.
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="green", edgecolors="blue", s=101)
+
+    # // Удаление осей.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # // Запуск.
+    plt.show()
+
+    stop_running = input("Остановка - ?: \n(y/n): ")
+    if stop_running.lower() == "n":
+        print("Блуждание окончилось.")
+        break
+    else:
+        continue
